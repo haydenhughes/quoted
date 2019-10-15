7683c0866418efb53661302de9ccad7dc4fb4099
Hayden Hughes <hayden@vulpes.chickenkiller.com>
2019-10-15 20:08:32 +1100

    Increase max quote length to 100 chars

--- quoted/models.py
+++ quoted/models.py
@@ -2,5 +2,5 @@ from mongoengine import DynamicDocument, StringField, URLField
 
 
 class Quote(DynamicDocument):
-    quote = StringField(max_length=20, required=True, unique=True)
+    quote = StringField(max_length=100, required=True, unique=True)
     uri = URLField()
