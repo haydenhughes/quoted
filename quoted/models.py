5cda5376d1d11f08ea15ded4f73379d97f93c3b1
Hayden Hughes <hayden@vulpes.chickenkiller.com>
2019-10-20 17:54:55 +1100

    Remove quote length limit

--- quoted/models.py
+++ quoted/models.py
@@ -12,7 +12,7 @@ class APIQuerySet(QuerySet):
 
 class Quote(DynamicDocument):
     meta = {'queryset_class': APIQuerySet}
-    quote = StringField(max_length=100, required=True, unique=True)
+    quote = StringField(required=True, unique=True)
     character = StringField()
     significance = StringField()
     themes = ListField(StringField())
